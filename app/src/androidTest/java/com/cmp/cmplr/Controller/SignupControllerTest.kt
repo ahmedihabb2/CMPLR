cc2918e836e36f2a6464202c1922b8d210814d4f
Mohamed <Mohamed_Khaled_Kamal@outlook.com>
2021-11-20 21:56:18 +0200

    unit testing and documantaiton done

---
+++ app/src/androidTest/java/com/cmp/cmplr/Controller/SignupControllerTest.kt
@@ -0,0 +1,12 @@
+package com.cmp.cmplr.Controller
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class SignupControllerTest {
+
+    @Test
+    fun getSignupData() {
+    }
+}
---
+++ app/src/test/java/com/cmp/cmplr/Controller/LoginControllerTest.kt
@@ -0,0 +1,5 @@
+package com.cmp.cmplr.Controller
+
+import org.junit.Assert.*
+
+class LoginControllerTest
