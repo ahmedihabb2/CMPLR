b7d54ad730ae42b8545e855bd2577161d6cf509b
Mohamed Khaled Mohamed <56936494+MohamedElhadidy0019@users.noreply.github.com>
2021-12-29 18:53:12 +0200

    changes to json parsing

--- app/src/main/java/com/cmp/cmplr/Model/HashtagModel.kt
+++ app/src/main/java/com/cmp/cmplr/Model/HashtagModel.kt
@@ -42,56 +42,69 @@ class HashtagModel {
         }
 
         var thebody: JsonObject? = response.body()
-        var jsonlist = thebody!!.getAsJsonArray("post")
-        Log.d("hashtag", "After json")
-        Log.d("hashtag", "size= " + jsonlist.size().toString())
-        Log.d("hashtag", "tag= " + hashtag)
+        var postsArry = thebody!!.getAsJsonArray("post")
+//        Log.d("hashtag", "After json")
+//        Log.d("hashtag", "size= " + postsArry.size().toString())
+//        Log.d("hashtag", "tag= " + hashtag)
+        if (postsArry != null) {
+            for (item in postsArry) {
+                val post = gson.fromJson(
+                    item.asJsonObject.getAsJsonObject("post"),
+                    Post::class.java
+                )
+                val blog = gson.fromJson(
+                    item.asJsonObject.getAsJsonObject("blog"),
+                    Blog::class.java
+                )
 
-        for (item in jsonlist) {
-            var single_post = item.asJsonObject.getAsJsonObject("post")
-            var single_blog = item.asJsonObject.getAsJsonObject("blog")
-
-            val content: String = single_post["content"].toString().replace("\"", "")
-            val date: String = single_post["date"].toString()
-            val is_liked: Boolean = single_post["is_liked"].toString().toBoolean()
-            val post_id: Int = single_post["post_id"].toString().toInt()
-            val source_content: String = single_post["source_content"].toString()
-            val state: String = single_post["state"].toString()
-            val type: String = single_post["type"].toString()
-            //val tags = MutableList<String>()
-            //var tags = listOf("df","sdf")
-            var tagsString = single_post["tags"].toString()
-            tagsString = tagsString.replace("[", "")
-            tagsString = tagsString.replace("]", "")
-            tagsString = tagsString.replace("\"", "")
-            var tags = tagsString.split(",")
-            val notes_count: Int = single_post["notes_count"].toString().toInt()
-
-            val post_data: Post = Post(
-                content,
-                date,
-                is_liked,
-                post_id,
-                source_content,
-                state,
-                tags,
-                type,
-                notes_count
-            )
-            //Log.d("back_content",)
-            val blog_data: Blog = Blog(
-                single_blog["avatar"].toString().replace("\"", ""),
-                single_blog["avatar_shape"].toString(),
-                single_blog["blog_id"].toString().toInt(),
-                single_blog["blog_name"].toString().replace("\"", ""),
-                single_blog["follower"].toString().toBoolean(),
-                single_blog["replies"].toString()
-            )
-            var homePostData: HomePostData = HomePostData(blog_data, post_data)
-            postList.add(homePostData)
-
-
+                postList.add(HomePostData(blog, post))
+            }
         }
+//        for (item in jsonlist) {
+//            var single_post = item.asJsonObject.getAsJsonObject("post")
+//            var single_blog = item.asJsonObject.getAsJsonObject("blog")
+//
+//            val content: String = single_post["content"].toString().replace("\"", "")
+//            val date: String = single_post["date"].toString()
+//            val is_liked: Boolean = single_post["is_liked"].toString().toBoolean()
+//            val post_id: Int = single_post["post_id"].toString().toInt()
+//            val source_content: String = single_post["source_content"].toString()
+//            val state: String = single_post["state"].toString()
+//            val type: String = single_post["type"].toString()
+//            //val tags = MutableList<String>()
+//            //var tags = listOf("df","sdf")
+//            var tagsString = single_post["tags"].toString()
+//            tagsString = tagsString.replace("[", "")
+//            tagsString = tagsString.replace("]", "")
+//            tagsString = tagsString.replace("\"", "")
+//            var tags = tagsString.split(",")
+//            val notes_count: Int = single_post["notes_count"].toString().toInt()
+//
+//            val post_data: Post = Post(
+//                content,
+//                date,
+//                is_liked,
+//                post_id,
+//                source_content,
+//                state,
+//                tags,
+//                type,
+//                notes_count
+//            )
+//            //Log.d("back_content",)
+//            val blog_data: Blog = Blog(
+//                single_blog["avatar"].toString().replace("\"", ""),
+//                single_blog["avatar_shape"].toString(),
+//                single_blog["blog_id"].toString().toInt(),
+//                single_blog["blog_name"].toString().replace("\"", ""),
+//                single_blog["follower"].toString().toBoolean(),
+//                single_blog["replies"].toString()
+//            )
+//            var homePostData: HomePostData = HomePostData(blog_data, post_data)
+//            postList.add(homePostData)
+//
+//
+//        }
 
 
 
