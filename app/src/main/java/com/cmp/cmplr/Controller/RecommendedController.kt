bd2a00bcc89d85d9ea2dc5e74476492eb275ec10
Ahmed Ihab <ahmedbaz14@gmail.com>
2022-01-01 02:40:19 +0200

    More functional documentation

--- app/src/main/java/com/cmp/cmplr/Controller/RecommendedController.kt
+++ app/src/main/java/com/cmp/cmplr/Controller/RecommendedController.kt
@@ -5,8 +5,19 @@ import com.cmp.cmplr.Model.ExploreModel
 import com.google.gson.Gson
 import com.google.gson.JsonArray
 
+/**
+ * Class for handling Explore page view and it's data
+ *
+ */
 class RecommendedController {
     val exploreModel = ExploreModel()
+
+    /**
+     * Member function used to parse the recommended blogs data that comes from Model
+     *
+     * @param token
+     * @return A List of RecommendedData class objects that contains the blog data
+     */
     suspend fun fetchRecommendedCont(token: String): ArrayList<RecommendedData> {
         val gson = Gson()
         val listOfBlogs: ArrayList<RecommendedData> = ArrayList()
